dad91bbfdc284d60c1f015134d9f8228b7d7326d
Игорь Рябухин <igorr1970@mail.ru>
2024-10-22 21:17:01 +0500

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,17 +1,13 @@
 from random import randint
 
-# Генерация случайного числа от 3 до 20
 n = randint(3, 20)
 print(f"Сгенерированное число: {n}")
 
-# Инициализация пустой строки для пароля
 result = ""
 
-# Поиск пар чисел
 for i in range(1, 21):
     for j in range(i + 1, 21):
-        if n % (i + j) == 0:  # Проверка кратности
-            result += str(i) + str(j)  # Добавление пары в результат
+        if n % (i + j) == 0:
+            result += str(i) + str(j)
 
-# Вывод результата
 print(f"Нужный пароль: {result}")
